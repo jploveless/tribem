81da79467b6fff61c7d82ae7ded1a8286930bb05
Jack Loveless <jloveles@smith.edu>
2021-01-27 12:01:33 -0500

    Remove debug statement

--- tribemx.m
+++ tribemx.m
@@ -247,7 +247,6 @@ if ~exist('remsp', 'var') % If there is no remote stress
    % Corrected traction is prescribed traction minus contribution from displacement b.c.
    elstress = ptrac - dstress(itrac);
    % Estimate slips that produce corrected traction
-   keyboard
    m = G.sp(itrac, esidx)\elstress;
    % Assemble full slip vector (prescribed and estimated)
    slip = zeros(3*tne, 1);
